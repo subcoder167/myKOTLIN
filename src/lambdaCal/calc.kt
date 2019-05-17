72c370482bf6f5641da1909058e78416cf994476
subcoder167 <42571438+subcoder167@users.noreply.github.com>
2019-05-17 16:11:26 +0530

    completed the program

--- src/lambdaCal/calc.kt
+++ src/lambdaCal/calc.kt
@@ -1,5 +1,37 @@
 package lambdaCal
 
+import java.util.*
+
 fun main() {
-    
+
+    val c = Scanner(System.`in`)
+    print("Enter 1st no: ")
+    val num1 = c.nextFloat()
+    print("Enter 2nd no: ")
+    val num2 = c.nextFloat()
+
+    print("Enter the function you want to perform(+,-,*,/) : ")
+    val choice = c.next()
+
+    val result = {x:Float,y:Float,ch:String -> when(ch){
+        "+" -> x add y
+        "-" -> x subtract y
+        "*" -> x multiply y
+        "/" -> x divide y
+        else -> 0.0f
+    }}
+    print("Result is : ${result(num1,num2,choice)}")
+}
+
+infix fun Float.add(other:Float):Float {
+    return this + other
+}
+infix fun Float.subtract(other:Float):Float {
+    return this - other
+}
+infix fun Float.multiply(other:Float):Float {
+    return this * other
+}
+infix fun Float.divide(other:Float):Float {
+    return this / other
 }
